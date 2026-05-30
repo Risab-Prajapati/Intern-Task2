b556597910ead144824c4cbad3c2fab50b33271a
Rishav Prajapati <rishavp063@gmail.com>
2026-05-30 18:58:14 +0545

    code modified

--- scripts.js
+++ scripts.js
@@ -9,13 +9,20 @@ menuToggle.addEventListener("click", () => {
   icon.classList.toggle("fa-times");
 });
 
+let button = document.querySelector(".btn");
 function validateForm() {
-  let name = document.forms["contactForm"]["name"].value;
-  let email = document.forms[contactForm]["email"];
+  let name = document.querySelector(".name").value;
+  let email = document.querySelector(".email").value;
 
-  if (name == "" || email == "") {
-    alert("Name and Email must be filled");
+  if (name === "" || email === "") {
+    alert("Input field must be filled");
     return false;
+  } else {
+    alert("Successfully submitted");
+    return true;
   }
-  return true;
 }
+
+button.addEventListener("click", () => {
+  validateForm();
+});
